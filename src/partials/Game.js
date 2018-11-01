eab823d98f2c0181ecaa93a7f80df0bab2e36a83
Jennifer <jen.yiu.90@gmail.com>
2018-11-01 10:48:47 -0700

    add append svg to render method of Game.js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,22 @@
+import { SVG_NS } from "../settings";
+
 export default class Game {
+  constructor(element, width, height) {
+    this.element = element;
+    this.width = width;
+    this.height = height;
 
-	constructor(element, width, height) {
-		this.element = element;
-		this.width = width;
-		this.height = height;
-	
-		// Other code goes here...
-	}
+    // Other code goes here...
+    this.gameElement = document.getElementById(this.element);
+  }
 
-	render() {
-		// More code goes here...
-	}
-
+  render() {
+    // More code goes here...
+    this.gameElement.innterHTML = ""; //empties out any html in this game element
+    let svg = document.createElementNS(SVG_NS, "svg"); // createElementNS means creating an element with that namespace
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewbox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+  }
 }
